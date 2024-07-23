2beb198356de87de2ef385ce02ba332114bc1b09
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-07-23 18:12:26 -0500

    breakup main.go with config.go

---
+++ cmd/api/config.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/ardanlabs/conf/v3"
+	"github.com/jalevin/gottl/internal/observability/logtools"
+	"github.com/jalevin/gottl/internal/web"
+)
+
+type Config struct {
+	conf.Version
+	Web  web.Config
+	Logs logtools.Config
+}
+
+var (
+	// Build information. Populated at build-time via -ldflags flag.
+	version = "dev"
+	commit  = "HEAD"
+	date    = "now"
+)
+
+func build() string {
+	short := commit
+	if len(commit) > 7 {
+		short = commit[:7]
+	}
+
+	return fmt.Sprintf("%s (%s) %s", version, short, date)
+}
+
+// ConfigFromCLI parses the CLI/Config file and returns a Config struct. If the file argument is an empty string, the
+// file is not read. If the file is not empty, the file is read and the Config struct is returned.
+func ConfigFromCLI() (*Config, error) {
+	cfg := Config{
+		Version: conf.Version{
+			Build: build(),
+			Desc:  "Rest API",
+		},
+	}
+
+	const prefix = "API"
+
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			os.Exit(0)
+		}
+		return &cfg, fmt.Errorf("parsing config: %w", err)
+	}
+
+	return &cfg, nil
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,26 +9,9 @@ import (
 	"os/signal"
 	"sync"
 
-	"github.com/ardanlabs/conf/v3"
 	"github.com/jalevin/gottl/internal/observability/logtools"
 	"github.com/jalevin/gottl/internal/web"
 )
-
-var (
-	// Build information. Populated at build-time via -ldflags flag.
-	version = "dev"
-	commit  = "HEAD"
-	date    = "now"
-)
-
-func build() string {
-	short := commit
-	if len(commit) > 7 {
-		short = commit[:7]
-	}
-
-	return fmt.Sprintf("%s (%s) %s", version, short, date)
-}
 
 func main() {
 	if err := run(); err != nil {
@@ -68,34 +51,4 @@ func run() error {
 	wg.Wait()
 
 	return nil
-}
-
-type Config struct {
-	conf.Version
-	Web  web.Config
-	Logs logtools.Config
-}
-
-// ConfigFromCLI parses the CLI/Config file and returns a Config struct. If the file argument is an empty string, the
-// file is not read. If the file is not empty, the file is read and the Config struct is returned.
-func ConfigFromCLI() (*Config, error) {
-	cfg := Config{
-		Version: conf.Version{
-			Build: build(),
-			Desc:  "Rest API",
-		},
-	}
-
-	const prefix = "API"
-
-	help, err := conf.Parse(prefix, &cfg)
-	if err != nil {
-		if errors.Is(err, conf.ErrHelpWanted) {
-			fmt.Println(help)
-			os.Exit(0)
-		}
-		return &cfg, fmt.Errorf("parsing config: %w", err)
-	}
-
-	return &cfg, nil
 }
