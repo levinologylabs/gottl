04b48daef725c16a1d38aad7534d384fb1fc34b5
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-08-14 11:06:25 -0800

    ci cache

--- workflows/main.go
+++ workflows/main.go
@@ -31,12 +31,36 @@ func (g *Workflows) TestEnv(source *dagger.Directory) *dagger.Container {
 		WithEnvVariable("POSTGRES_USER", "postgres").
 		AsService()
 
+	/* goMod := ci.dag.Host().File(projectPath("backend", "go.mod")) */
+	/* goSum := ci.dag.Host().File(projectPath("backend", "go.sum")) */
+	/* return c.WithFile("src/go.mod", goMod). */
+	/* 	WithFile("src/go.sum", goSum). */
+	/* 	WithMountedCache("/go/pkg/mod", ci.dag.CacheVolume("go-mod-121")). */
+	/* 	WithEnvVariable("GOMODCACHE", "/go/pkg/mod"). */
+	/* 	WithExec([]string{"go", "mod", "download"}) */
+
 	golang := g.dag.Container().
 		From(imageGolang).
 		WithServiceBinding("postgres", postgres).
 		WithEnvVariable("GOTTL_POSTGRES_HOST", "postgres").
-		WithDirectory("/src/", source).
 		WithWorkdir("/src/")
+
+	// Mount Go Module Files
+	golang = golang.
+		WithFile("/src/go.mod", source.File("go.mod")).
+		WithFile("/src/go.sum", source.File("go.sum")).
+		WithMountedCache("/go/pkg/mod", g.dag.CacheVolume("go-mod-121")).
+		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
+		WithExec([]string{"go", "mod", "download"})
+
+	// Mount source code and build cache
+	golang = golang.
+		WithDirectory("/src/", source, dagger.ContainerWithDirectoryOpts{
+			// Exclude already copied files and directories that are not relevant to Go
+			Exclude: []string{"go.mod", "go.sum", ".git/**", ".task/**"},
+		}).
+		WithMountedCache("/go/build-cache", g.dag.CacheVolume("go-build-121")).
+		WithEnvVariable("GOCACHE", "/go/build-cache")
 
 	return golang
 }
