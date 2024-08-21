86a2529d2ffe3448560678c59002f5cfb91473f1
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-08-21 12:48:16 -0500

    remove extra comma

--- .scaffold/domain/templates/migrations.sql
+++ .scaffold/domain/templates/migrations.sql
@@ -4,8 +4,7 @@ CREATE TABLE IF NOT EXISTS {{ .Computed.sql_table }} (
     -- table column defaults
     id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    -- Insert Additional Columns Here
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose StatementEnd
