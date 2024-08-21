fe21af58c13b001162f6be82508e85dabb23fd10
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-08-21 12:47:25 -0500

    generate migration file

---
+++ .scaffold/domain/templates/migrations.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS {{ .Computed.sql_table }} (
+    -- table column defaults
+    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    -- Insert Additional Columns Here
+);
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS {{ .Computed.sql_table }};
+
+-- +goose StatementEnd
